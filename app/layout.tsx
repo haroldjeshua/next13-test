dad513949ea1d5364a9f9fa2ab43e9dd136e909e
Harold Jeshua Varde <harvardii15@gmail.com>
2022-11-02 17:58:52 +0800

    notes page + pb api func

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-head-element */
 
 import Link from "next/link";
+import './globals.css';
 
 export default function RootLayout({
   children,
@@ -16,8 +17,8 @@ export default function RootLayout({
             <Link href="/">Home</Link>
             <Link href="/notes">Notes</Link>
           </nav>
+          {children}
         </main>
-        {children}
       </body>
     </html>
   );
